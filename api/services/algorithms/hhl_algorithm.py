81baff78988d6e24e4f54a92f0efc3b34d35f7ed
Julian Obst <obstjn@studi.informatik.uni-stuttgart.de>
2021-09-28 15:26:53 +0200

    Changed name of Encoding folder to encoding.

--- api/services/algorithms/hhl_algorithm.py
+++ api/services/algorithms/hhl_algorithm.py
@@ -3,7 +3,7 @@ from qiskit.circuit.quantumcircuit import QuantumCircuit
 from qiskit.algorithms.linear_solvers.hhl import HHL
 import numpy as np
 
-from api.services.Encodings.amplitude_encoding import AmplitudeEncoding
+from api.services.encodings.amplitude_encoding import AmplitudeEncoding
 
 class HHLAlgorithm:
     @classmethod
