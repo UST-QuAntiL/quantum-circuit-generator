592a4d9356d036f73412ee05b6d74868f373b8a5
Julian Obst <obstjn@studi.informatik.uni-stuttgart.de>
2022-05-11 21:15:25 +0200

    VQE adjustment.

--- api/services/algorithms/vqe_algorithm.py
+++ api/services/algorithms/vqe_algorithm.py
@@ -14,29 +14,8 @@ class VQEAlgorithm:
         :param barriers: boolean flag, wether or not to insert barriers
         :return: OpenQASM Circuit
 
-        Creates the circuit of the quantum fourier transform with n_qubits.
-        Suppose |x> = |x1 x2 ... xn > with x1 being the most significant bit.
-        Since in qiskit, the most significant qubit is qn, we have:
-        |x1 x2 ... xn > = |qn ... q1 >
+        Description
         """
 
-        qft = QuantumCircuit(n_qubits)
-        for i in reversed(range(n_qubits)):
-            if barriers:  # Add barrier if specified
-                qft.barrier()
-            qft.h(i)
-            for j in range(i):
-                # control is down 1, 2,...  --> i-(j+1)
-                qft.cp(
-                    2 * np.pi / 2 ** (j + 2), i - (j + 1), i
-                )  # cp(phase, control, target)
-        # swaps
-        if barriers:
-            qft.barrier()
-        for i in range(n_qubits // 2):
-            qft.swap(i, n_qubits - i - 1)
 
-        if inverse:
-            qft = qft.inverse()
-
-        return qft
+        return None
